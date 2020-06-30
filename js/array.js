ae7beb334f5ecb9854b825e6990b003314e207ab
edavilaz <edavilaz@gmail.com>
2020-06-30 10:54:10 -0500

    arreglos

---
+++ js/array.js
@@ -0,0 +1,14 @@
+var amigos = ["Carlos", "Cesar", "Alex"];
+// para imprimir 
+document.write(amigos[0]);
+document.write("<br>");
+
+// imprimiría undefined porque no existe nada en la posición 9
+document.write(amigos[9]);
+document.write("<br>");
+
+// Cambiar un dato específico
+amigos[0] ="Arturo";
+document.write(amigos[0]);
+document.write("<br>");
+
