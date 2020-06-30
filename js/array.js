f2f2df91a1062da0a8b3ab7a72700b97c42dd6ca
edavilaz <edavilaz@gmail.com>
2020-06-30 14:55:28 -0500

    métodos y propiedades de los arreglos

--- js/array.js
+++ js/array.js
@@ -12,3 +12,35 @@ amigos[0] ="Arturo";
 document.write(amigos[0]);
 document.write("<br>");
 
+// Longitud del array
+document.write("El tamaño del arreglo es: " +  amigos.length);
+document.write("<br>");
+
+// Crear nuevo elemento
+
+amigos[amigos.length] = "Fernando";
+document.write(amigos);
+document.write("<br>");
+
+//otra forma
+amigos.push("Javier", "Ramon","Oscar");
+document.write(amigos);
+document.write("<br>");
+//Eliminar el último elemento
+amigos.pop();
+document.write(amigos);
+document.write("<br>");
+
+// unir dos arreglos en un tercero
+amigos2 = ["Luis", "Fabian", "Raul"];
+amigos3 = amigos.concat(amigos2);
+document.write(amigos3);
+document.write("<br>");
+
+// Para cambiar la separación de los elementos, pondremos espacio : espacio
+document.write(amigos.join(" : "));
+document.write("<br>");
+
+// ordenar un array
+document.write(amigos3.sort());
+document.write("<br>");
