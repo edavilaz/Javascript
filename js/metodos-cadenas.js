ad0e471dd075aa62fe82dcd9dc4dd24256e3f353
edavilaz <edavilaz@gmail.com>
2020-07-01 15:35:20 -0500

    metodos-cadenas de texto

---
+++ js/metodos-cadenas.js
@@ -0,0 +1,53 @@
+//length
+// var texto = "Enrique Davila";
+// var nuevoTexto = texto.length;
+// document.write(nuevoTexto);
+// document.write("<br");
+
+//substring (indice)
+// var texto = "Enrique Davila";
+// var nuevoTexto = texto.substring(7);
+// document.write(nuevoTexto);
+// document.write("<br");
+
+// substr (inicio, fin)
+// var texto = "Enrique Davila";
+// var nuevoTexto = texto.substr(0,7);
+// document.write(nuevoTexto);
+// document.write("<br");
+
+// indexOf("e") busca desde le inicio
+// var texto = "Enrique Davila";
+// var nuevoTexto = texto.indexOf("a");
+// document.write(nuevoTexto);
+// document.write("<br");
+
+// lastIndexOf ("e") busca desde el final
+// var texto = "Enrique Davila";
+// var nuevoTexto = texto.lastIndexOf("a");
+// document.write(nuevoTexto);
+// document.write("<br");
+
+// Replace para cambiar un texto, primero el texto a cambiar y luego el texto nuevo
+// var texto = "Enrique Davila";
+// var nuevoTexto = texto.replace("Davila", "Zuazo");
+// document.write(nuevoTexto);
+// document.write("<br");
+
+// toUpperCase a mayúsculas
+// var texto = "Enrique Davila";
+// var nuevoTexto = texto.toUpperCase();
+// document.write(nuevoTexto);
+// document.write("<br");
+
+// toLowerCase a minúsculas
+// var texto = "Enrique Davila";
+// var nuevoTexto = texto.toLowerCase();
+// document.write(nuevoTexto);
+// document.write("<br");
+
+// toUpperCase a mayúsculas
+// var texto = "Enrique Davila";
+// var nuevoTexto = texto.toUpperCase;
+// document.write(nuevoTexto);
+// document.write("<br");
