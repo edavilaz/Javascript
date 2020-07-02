dac9355aa7e61b7bfec02f2405aafa1683dc1d52
edavilaz <edavilaz@gmail.com>
2020-07-02 12:10:10 -0500

    estiloselementos

---
+++ js/estilodeelementos.js
@@ -0,0 +1,8 @@
+var h1 = document.getElementById("h1");
+h1.className = "display-1 bg-warning text-center text-danger";
+
+// h1.setAttribute ("class","display-2 text-right text-success bg-danger");
+
+// mediante style
+
+// h1.style.color = "orange";
